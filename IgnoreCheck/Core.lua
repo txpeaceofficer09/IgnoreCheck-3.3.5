dfeaad6ec2002f96d7093b503c70a4f674293843
txpeaceofficer09 <james.mccaughey@gmail.com>
2025-12-23 04:40:47 -0600

    Cleaned up.

--- IgnoreCheck/Core.lua
+++ IgnoreCheck/Core.lua
@@ -1,39 +1,43 @@
 local f = CreateFrame("Frame")
 
 local function IsOnIgnore(name)
-	local numIgnore = GetNumIgnore()
+	local i = 1
 
-	for i=1,numIgnore,1 do
+	while GetIgnoreName(i) ~= nil do
 		if GetIgnoreName(i) == name then
 			return true
 		end
+
+		i = i + 1
 	end
 
 	return false
 end
 
-local function OnEvent(self, event, ...)
-	print(event)
+local function printmsg(msg)
+	print(("[|cffaaffIgnoreCheck|r]: %s"):format(msg))
+end
 
+local function OnEvent(self, event, ...)
 	local numParty, numRaid = GetNumPartyMembers(), GetNumRaidMembers()
 	local memberName = nil
 
 	if numRaid > 0 then
 		for i=1,numRaid,1 do
 			memberName = UnitName("raid"..i)
+
 			if IsOnIgnore(memberName) then
-				print(("[|cffffaa00IgnoreCheck|r]: %s is on your ingore list."):format(memberName))
-			else
-				print("Not on ignore: "..memberName)
+				--print(("[|cffffaa00IgnoreCheck|r]: %s is on your ingore list."):format(memberName))
+				printmsg(memberName.." is on your ignore list.")
 			end
 		end
 	elseif numParty > 0 then
 		for i=1,numRaid,1 do
 			memberName = UnitName("party"..i)
+
 			if IsOnIgnore(memberName) then
-				print(("[|cffffaa00IgnoreCheck|r]: %s is on your ignore list."):format(memberName))
-			else
-				print("Not on ignore: "..memberName)
+				--print(("[|cffffaa00IgnoreCheck|r]: %s is on your ignore list."):format(memberName))
+				printmsg(memberName.." is on your ignore list.")
 			end
 		end
 	end
